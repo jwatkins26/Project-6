be9dd6ce83c7637db57073f5f145a99e1a9517f0
John Watkins <johnwwatkins3@gmail.com>
2020-07-08 05:43:41 -0500

    1st commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace SemesterProject06
+{
+    partial class SemesterProject06
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.bdp = new System.Windows.Forms.DateTimePicker();
+            this.fdp = new System.Windows.Forms.DateTimePicker();
+            this.OutputLabel = new System.Windows.Forms.Label();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.ExecuteButton = new System.Windows.Forms.Button();
+            this.DaysLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter your name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter your birthdate:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Enter a date in the future:";
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(171, 57);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(247, 22);
+            this.nameBox.TabIndex = 3;
+            // 
+            // bdp
+            // 
+            this.bdp.Location = new System.Drawing.Point(171, 110);
+            this.bdp.Name = "bdp";
+            this.bdp.Size = new System.Drawing.Size(247, 22);
+            this.bdp.TabIndex = 4;
+            this.bdp.ValueChanged += new System.EventHandler(this.bdp_ValueChanged);
+            // 
+            // fdp
+            // 
+            this.fdp.Location = new System.Drawing.Point(188, 164);
+            this.fdp.Name = "fdp";
+            this.fdp.Size = new System.Drawing.Size(247, 22);
+            this.fdp.TabIndex = 5;
+            this.fdp.ValueChanged += new System.EventHandler(this.fdp_ValueChanged);
+            // 
+            // OutputLabel
+            // 
+            this.OutputLabel.AutoSize = true;
+            this.OutputLabel.Location = new System.Drawing.Point(15, 228);
+            this.OutputLabel.Name = "OutputLabel";
+            this.OutputLabel.Size = new System.Drawing.Size(145, 17);
+            this.OutputLabel.TabIndex = 6;
+            this.OutputLabel.Text = "Nothing entered yet...";
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(436, 406);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 32);
+            this.ExitButton.TabIndex = 7;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // ExecuteButton
+            // 
+            this.ExecuteButton.Location = new System.Drawing.Point(355, 406);
+            this.ExecuteButton.Name = "ExecuteButton";
+            this.ExecuteButton.Size = new System.Drawing.Size(75, 32);
+            this.ExecuteButton.TabIndex = 8;
+            this.ExecuteButton.Text = "Execute";
+            this.ExecuteButton.UseVisualStyleBackColor = true;
+            this.ExecuteButton.Click += new System.EventHandler(this.ExecuteButton_Click);
+            // 
+            // DaysLabel
+            // 
+            this.DaysLabel.AutoSize = true;
+            this.DaysLabel.Location = new System.Drawing.Point(15, 249);
+            this.DaysLabel.Name = "DaysLabel";
+            this.DaysLabel.Size = new System.Drawing.Size(38, 17);
+            this.DaysLabel.TabIndex = 9;
+            this.DaysLabel.Text = "days";
+            this.DaysLabel.Visible = false;
+            // 
+            // SemesterProject06
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 450);
+            this.Controls.Add(this.DaysLabel);
+            this.Controls.Add(this.ExecuteButton);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.OutputLabel);
+            this.Controls.Add(this.fdp);
+            this.Controls.Add(this.bdp);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "SemesterProject06";
+            this.Text = "Semester Project - 06";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.DateTimePicker bdp;
+        private System.Windows.Forms.DateTimePicker fdp;
+        private System.Windows.Forms.Label OutputLabel;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Button ExecuteButton;
+        private System.Windows.Forms.Label DaysLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SemesterProject06
+{
+    public partial class SemesterProject06 : Form
+    {
+        public SemesterProject06()
+        {
+            InitializeComponent();
+        }
+
+        private void ExecuteButton_Click(object sender, EventArgs e)
+        {
+            
+            string name = nameBox.Text;
+            DateTime today = DateTime.Today;
+            DateTime birthDate = bdp.Value.Date;
+            DateTime futureDate = fdp.Value.Date;
+            //TimeSpan future = futureDate - birthDate;
+            DateTime nextBday = new DateTime(today.Year + 1, birthDate.Month, birthDate.Day);
+            TimeSpan yearsOld = today - birthDate;
+           
+            int age = futureDate.Year - birthDate.Year;
+            TimeSpan future = futureDate - today;
+            int days = future.Days;
+
+            OutputLabel.Text = "Hello " + name + " you will be " + age + " years old on " + futureDate.ToString("MMMM dd, yyyy");
+            DaysLabel.Visible = true;
+            DaysLabel.Text = "A date that is " + days +" days from now.";
+            DaysLabel.Update();
+        }
+
+        private void fdp_ValueChanged(object sender, EventArgs e)
+        {
+            DateTime today = DateTime.Today;
+
+            if (fdp.Value.Date < today)
+            {
+                MessageBox.Show("Error - Date must be in the future.");
+                fdp.Value = today;
+            }
+        }
+
+        private void bdp_ValueChanged(object sender, EventArgs e)
+        {
+            DateTime today = DateTime.Today;
+            if (bdp.Value.Date > today)
+            {
+                MessageBox.Show("Error - Date must be in past");
+                bdp.Value = today;
+
+
+             }
+                  
+            
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
